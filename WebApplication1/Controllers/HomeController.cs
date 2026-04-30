525ee70ce3419d7f61a366ff9e8221e6edfd552f
William <williamverde10@gmail.com>
2026-04-30 10:21:55 -0400

    Added: home controller

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : Controller
+    {
+       
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Layouts", "index.html");
+            string htmlContent = await System.IO.File.ReadAllTextAsync(filePath);
+
+            return Content(htmlContent, "text/html", System.Text.Encoding.UTF8);
+        }
+    }
+}
