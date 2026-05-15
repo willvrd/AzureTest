743d50afdff70d439a1fd025019526b30463d55e
William <williamverde10@gmail.com>
2026-05-15 16:45:42 -0400

    Added: Post Extension

---
+++ WebApplication1/Extensions/PostExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Extensions
+{
+    public static class PostExtension
+    {
+        public static string? ToFullUrl(this string? relativePath, IConfiguration config)
+        {
+            if (string.IsNullOrEmpty(relativePath)) return null;
+
+            var baseUrl = config["BlobContainers:BlobBaseUrl"]?.TrimEnd('/');
+            var container = config["BlobContainers:ContainerName"]?.TrimStart('/').TrimEnd('/');
+
+            return $"{baseUrl}/{container}/{relativePath.TrimStart('/')}";
+        }
+    }
+}
