5cdea6c3a0eb401d6ba5f66ee3f22e350ba67a1f
William <williamverde10@gmail.com>
2026-05-29 16:38:13 -0400

    Added: slug | StringExtension | Core

---
+++ WebApplication1/Modules/Core/Extensions/StringExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WebApplication1.Core.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ToSlug(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return string.Empty;
+
+            string slug = value.ToLowerInvariant();
+
+            slug = Regex.Replace(slug, @"[áàäâã]", "a");
+            slug = Regex.Replace(slug, @"[éèëê]", "e");
+            slug = Regex.Replace(slug, @"[íìïî]", "i");
+            slug = Regex.Replace(slug, @"[óòöôõ]", "o");
+            slug = Regex.Replace(slug, @"[úùüû]", "u");
+            slug = Regex.Replace(slug, @"ñ", "n");
+
+            //Delete specials characters
+            slug = Regex.Replace(slug, @"[^a-z0-9\s-]", "");
+
+            // Replace multiple consecutive spaces or hyphens with a single hyphen
+            slug = Regex.Replace(slug, @"[\s-]+", "-").Trim('-');
+
+            return slug;
+        }
+    }
+}
